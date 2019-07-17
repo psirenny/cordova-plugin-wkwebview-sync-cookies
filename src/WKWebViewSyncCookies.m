9b6c5e272ff0f11d85e6572a4ccdd2bf2cd934e8
Aditya Karnam <akarnam37@gmail.com>
2019-07-17 14:53:09 -0500

    fix: cookie sync support for ios 11

--- src/WKWebViewSyncCookies.m
+++ src/WKWebViewSyncCookies.m
@@ -4,32 +4,51 @@
 @implementation WKWebViewSyncCookies
 
 - (void)sync:(CDVInvokedUrlCommand *)command {
-  @try {
-    NSString *urlHttpMethod = command.arguments[0];
-    NSString *urlString = command.arguments[1];
-    NSURL *url = [NSURL URLWithString:urlString];
+  WKWebView* wkWebView = (WKWebView*) self.webView;
+  if (@available(iOS 11.0, *)) {
+    NSString *domain = command.arguments[2];
+    NSString *path = command.arguments[3];
+    
+    NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+    [cookieProperties setObject:@"foo" forKey:NSHTTPCookieName];
+    [cookieProperties setObject:@"bar" forKey:NSHTTPCookieValue];
+    [cookieProperties setObject:domain forKey:NSHTTPCookieDomain];
+    [cookieProperties setObject:domain forKey:NSHTTPCookieOriginURL];
+    [cookieProperties setObject:path forKey:NSHTTPCookiePath];
+    NSHTTPCookie * cookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
 
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
-    [urlRequest setHTTPMethod:urlHttpMethod];
-    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [wkWebView.configuration.websiteDataStore.httpCookieStore setCookie:cookie completionHandler:^{NSLog(@"Cookies synced");}];
 
-    NSURLSession *urlSession = [NSURLSession sharedSession];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+  } else {
+    @try {
+      NSString *urlHttpMethod = command.arguments[0];
+      NSString *urlString = command.arguments[1];
+      NSURL *url = [NSURL URLWithString:urlString];
 
-    [[urlSession dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable urlResponse, NSError * _Nullable error) {
-      CDVPluginResult *result;
+      NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+      [urlRequest setHTTPMethod:urlHttpMethod];
+      [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
 
-      if (error) {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
-      } else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-      }
+      NSURLSession *urlSession = [NSURLSession sharedSession];
 
+      [[urlSession dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable urlResponse, NSError * _Nullable error) {
+        CDVPluginResult *result;
+
+        if (error) {
+          result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
+        } else {
+          result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        }
+
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+      }] resume];
+    }
+    @catch (NSException *exception) {
+      CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:exception.reason];
       [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    }] resume];
-  }
-  @catch (NSException *exception) {
-    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:exception.reason];
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }
   }
 }
 
